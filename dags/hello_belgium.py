6866927ece2965dfa44b0b303ccdbb4729b1365c
charles verleyen <charles@astrafy.io>
2025-03-19 20:24:05 +0100

    add dummy dags

---
+++ dags/hello_belgium.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+# Define the Python function
+def hello_france():
+    print("Hello, france!")
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 1, 1),
+    'retries': 1,
+}
+
+# Define the DAG
+dag = DAG(
+    'hello_france_dag',
+    default_args=default_args,
+    description='A simple Hello france DAG',
+    schedule_interval=None,  # Runs only once (manual trigger)
+)
+
+# Define the task using the PythonOperator
+hello_task = PythonOperator(
+    task_id='hello_france_task',
+    python_callable=hello_france,
+    dag=dag,
+)
+
+# Set the task order
+hello_task
---
+++ hello_belgium.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+# Define the Python function
+def hello_belgium():
+    print("Hello, belgium!")
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2025, 1, 1),
+    'retries': 1,
+}
+
+# Define the DAG
+dag = DAG(
+    'hello_belgium_dag',
+    default_args=default_args,
+    description='A simple Hello belgium DAG',
+    schedule_interval=None,  # Runs only once (manual trigger)
+)
+
+# Define the task using the PythonOperator
+hello_task = PythonOperator(
+    task_id='hello_belgium_task',
+    python_callable=hello_belgium,
+    dag=dag,
+)
+
+# Set the task order
+hello_task
